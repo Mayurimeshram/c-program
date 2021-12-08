ed12bf065fa71e5706585d42fb5dc108bc5aff9e
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-08 20:57:50 +0530

    Add files via upload

---
+++ REVERSE.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,revere=0,i;
+printf(" enter a number:");
+scanf("%d",&n);
+while(n!=0)
+{
+i=n%10;
+reverse=reverse*10+i,n\10;
+printf("reverse number:%d;i);
+return 0;
+}
