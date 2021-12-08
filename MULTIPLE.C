94eb9515825aab3756b87c9015b0f1f00633f186
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-08 20:40:21 +0530

    Add files via upload

---
+++ MULTIPLE.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2;
+printf("enter the two value");
+scanf("%d",&n1,&n2);
+if(n1%n2==0);
+{
+printf("is it multiple%d",n1,n2);
+}
+else;
+{
+printf(" it is not multiple%d",n1,n2);
+{
+getch();
+}
+
