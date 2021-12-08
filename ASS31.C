f24f969f560965a66ca4d7d545f260f483c74edd
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-08 22:38:55 +0530

    Add files via upload

---
+++ ASS31.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a,b;
+clrscr();
+printf("enter any two number in pair\n");
+printf("enter fisrt number in pair\n");
+scanf("%d",&a);
+printf("enter sceond number in pair\n");
+scanf("%d",&b);
+for(i=0;i<=0;i++)
+{
+if(a<b)
+{
+printf("enter number is ascening order\n");
+}
+else
+printf("enter number is decending order\n");
+}
+getch();
+}
