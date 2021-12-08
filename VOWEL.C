4c39e315f5ecccc7db02ba234e77d8ba6a70a9fe
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-08 20:32:30 +0530

    Add files via upload

---
+++ VOWEL.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n1;
+printf("enter any character to check it is vowel");
+scanf("%d",&n1);
+switch(n1)
+{
+case'a':
+printf("enter character is vowel");
+break;
+case'e':
+printf("enter character is vowel");
+break;
+case'i':
+printf("enter character is vowel");
+break;
+case'o':
+printf("enter character is vowel");
+break;
+case'u':
+printf("enter character is vowel");
+break;
+default:
+printf("enter character is vowel ");
+break;
+getch();
+}
+
+ }
