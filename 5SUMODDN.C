70421fd17a10316e3403cf6c19dd2a7c1835c899
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-08 20:50:57 +0530

    Add files via upload

---
+++ 5SUMODDN.C
@@ -0,0 +1,22 @@
+// wrie a c program that reads a 5 numbers and //
+// sum of all odd values between them//
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,num=5,total=0;
+printf("enter fisrt number");
+scanf("%d",&n1);
+printf(" enter second number");
+scanf("%d",&n2);
+printf("enter third number");
+scanf("%d",&n3);
+printf("enter fourth number");
+scanf("%d",&n4);
+printf("enter fiveth number");
+scanf("%d",&n5);
+for(i=0;i<=5;i++)
+{
+if((num[i]%2)!=0)
+{
+total=
