9f5748c7068cf0f2dc40bbf6d9f36cb517d5ba5d
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-08 20:33:05 +0530

    Add files via upload

---
+++ ASS30.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,s;
+clrscr();
+printf("enter the even number");
+scanf("%d",&i);
+for(i=0;i<=10;i++)
+{
+if(i%2==0)
+s=s*i;
+}
+{
+printf("enter the square of each one of even numbers ",i,s);
+}
+getch();
+}
