30f402af75c6e4b2e24599a083169ea6b30ab412
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-09 00:01:11 +0530

    Add files via upload

---
+++ ASS29.C
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n1,n2,n3,n4,n5,total=0;
+clrscr();
+printf("enter fisrt number");
+scanf("%d",&n1);
+printf("enter second number");
+scanf("%d",&n2);
+printf("enter third number");
+scanf("%d",&n3);
+printf("enter fourth number");
+scanf("%d",&n4);
+printf("enter fiveth number");
+scanf("%d",&n5);
+for(i=0;i<=0;i++);
+{
+if(n1%2!=0)
+total=total+n1;
+}
+{
+if(n2%2!=0)
+total=total+n2;
+}
+{
+if(n3%2!=0)
+total=total+n3;
+}
+{
+if(n4%2!=0)
+total=total+n4;
+}
+{
+if(n5%2!=0)
+total=total+n5;
+}
+{
+printf(" enter the sum of all odd values between them%d",total);
+}
+getch();
+}
