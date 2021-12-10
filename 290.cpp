df10ff6e2b6a047dbb319b567840385d629ea14d
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-10 23:32:13 +0530

    Add files via upload

---
+++ 290.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r, c;
+	for(r=5;r>=1;r--)
+	{
+		for(c=1;c<=r;c++)
+		{
+			printf("*",c);
+			{
+				printf("\n");
+			}
+		}
+
+	}
+	return 0;
+}
+
