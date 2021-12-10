0591e299cf8e343bc6082a3213bf41d7c047fe6b
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-11 00:01:40 +0530

    Add files via upload

---
+++ ARRAY2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[3];
+a[0]=2;
+a[1]=3;
+a[2]=4;
+
+printf("roll no1 %d\n",a[0]);
+printf("roll no2 %d\n",a[1]);
+printf("roll no3 %d\n",a[2]);
+
+getch();
+}
