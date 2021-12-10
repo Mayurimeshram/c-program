95c3123790726586a08f08ba9ab21aadcd1b2d82
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-11 00:05:40 +0530

    Add files via upload

---
+++ ASS32.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+char password;
+clrscr();
+printf("enter a password until it is correct");
+scanf("%d",&i);
+{
+if(password==1234)
+{
+printf("correct password");
+}
+else
+{
+printf("incorrect password\n");
+}
+printf("\n");
+}
+getch();
+}
