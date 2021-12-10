1180dce072768e0900d949289fb60602afd238f3
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-10 23:45:42 +0530

    Add files via upload

---
+++ 279.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char r,c,a='A';
+clrscr();
+for(r='A';r<='D';r++)
+{
+for(c='A';c<='D';c++)
+{
+printf("%c",r);
+}
+printf("\n");
+}
+getch();
+}
---
+++ 281.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char r,c,a='A';
+clrscr();
+for(r='A';r<='E';r++)
+{
+for(c='A';c<='E';c++)
+{
+printf("%c",c);
+}
+printf("\n");
+}
+getch();
+}
