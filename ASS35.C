05908ac5264481acca7ed59555a5fd1153458d96
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-11 00:07:16 +0530

    Add files via upload

---
+++ ASS35.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i,sum=0;
+printf("enter fisrt number\n");
+scanf("%d",&a);
+printf("enter second number\n");
+scanf("%d",&b);
+if(i%17!=0)
+{
+printf("sum of all number not divided by 17\n");
+sum=sum+i;
+}
+getch();
+}
