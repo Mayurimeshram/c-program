72ebddfdff5248192dbcd57e74e1e7f49bef43f0
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-11 00:00:06 +0530

    Add files via upload

---
+++ ARRAY1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[3];
+int i=0;
+printf("enter any 3 number");
+for(i=0;i<=3;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=3;i++)
+{
+printf("%d",a[i]);
+}
+getch();
+}
