47f263cc21606d1896448a1ca91c1d22ed17f15b
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-12 17:31:47 +0530

    Add files via upload

---
+++ NDIGIT.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,s,d1;
+clrscr();
+printf("enter any n digit number");
+scanf("%d",&num);
+s=0;
+while(num>0)
+{
+d1=num%10;
+s=s+d1;
+printf("%d",s);
+num=num/10;
+}
+getch();
+}
