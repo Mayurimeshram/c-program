68ef9d91b45a3b32e4afe68c0658abfa4b854000
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-12 17:16:48 +0530

    Add files via upload

---
+++ PNNUMBER.C
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n1,n2,n3,n4,n5,c=0, total=0;
+printf(" enter fisrt number\n");
+scanf("%d",&n1);
+printf(" enter  second number\n");
+scanf("%d",&n2);
+printf(" enter third number\n");
+scanf("%d",&n3);
+printf(" enter fourth number\n");
+scanf("%d",&n4);
+printf(" enter  fiveth number\n");
+scanf("%d",&n5);
+for(i=o;i>=0;i++)
+if(n1>=0)
+{
+c++;
+total=total+i;
+}
+if(n2>=0)
+{
+c++;
+total=total+i;
+}
+if(n3>=0)
+{
+c++;
+total=total+i;
+}
+if(n4>=0)
+{
+c++;
+total=total+i;
+}
+if(n5>=0)
+{
+c++;
+total=total+i;
+}
+{
+printf(" enter the number is positive\n",c);
+}
+else;
+{
+printf(" enter the number is negative\n",c);
+}
+
+getch();
+}
