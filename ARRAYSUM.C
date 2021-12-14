f4b5e8ef31e1e1056544ba2215c57944bd171618
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-14 12:44:46 +0530

    Add files via upload

---
+++ ARRAYSUM.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int marks[5]={22,33,44,55,66};
+int i;
+int sum=0;
+for(i=0;i<5;i++)
+sum=sum+marks[i];
+{
+printf(" enter marks%d\n",sum);
+}
+getch();
+}
