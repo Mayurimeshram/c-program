b97f84fe872a4081dbea7eb7f105b80f3dd1662b
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-15 22:33:07 +0530

    Add files via upload

---
+++ FUNCTION.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf(" mayuri meshram\n");
+printf(" mayuri\n");
+printf(" mayuri meshram\n");
+printf(" mona\n");
+printf(" mayuri meshram\n");
+printf("mayuri\n");
+printf("mayu\n");
+getch();
+}
