b464957bd3b6ec8d09e8280f77698679842f35d1
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-15 22:30:10 +0530

    Add files via upload

---
+++ MATRIX1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int arr[3][3]={(1,3,4),(4,5,6),(7,8,9)};
+int r,c;
+clrscr();
+for(r=0;r<=3;r++);
+{
+for(c=0;c<=3;c++);
+{
+printf("%d",arr[3][3]);
+}
+printf("\n");
+}
+getch();
+}
