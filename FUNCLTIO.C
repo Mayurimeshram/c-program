e9ee22d89f4c4730119640f27dcb66a9f3940de7
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-15 22:33:38 +0530

    Add files via upload

---
+++ FUNCLTIO.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void display()
+{
+int display;
+printf(" mayuri meshram\n");
+printf(" mona\n");
+}
+ void main()
+ {
+ printf(" mayuri\n");
+ printf(" mayu\n");
+ display();
+ printf(" hi\n");
+ display();
+ printf(" laddu\n");
+ printf(" bestfriend\n");
+ display();
+ getch();
+ }
+
+
