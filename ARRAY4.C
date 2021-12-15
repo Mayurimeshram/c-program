0a46bd44aec90b8ded7d0c924f5412c31837f101
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-15 22:27:00 +0530

    Add files via upload

---
+++ ARRAY4.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int arr[3][2];
+int r,c;
+for(r=0;r<3;r++)
+{
+for(c=0;c<2;c++)
+{
+printf(" enter rollnumber and marks\n");
+scanf("%d",arr[3][2]);
+}
+}
+
+printf(" enter rollnumber and marks\n");
+{
+for(r=0;r<3;r++)
+{
+for(c=0;c<2;c++);
+{
+printf("%d",arr[3][2]);
+}
+printf("\n");
+}
+getch();
+}
+}
