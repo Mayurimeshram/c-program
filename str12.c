f4f72f9413ea0d43caaf5aeedd94b1bf1ce1303e
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-17 17:01:58 +0530

    Add files via upload

---
+++ str12.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+int main()
+{
+    char name[10]="Amit";
+    char name1[10]="Amit";
+    {
+	
+    	int res=strcmp(name,name1);
+    	if(strcmp(name,name1)==0)
+    	
+    	{
+    		printf(" string is equal");
+		}
+    	 else
+    	 {
+    	 	printf(" string is not equal");
+		 }
+}
+}
