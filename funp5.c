d19bc895bef5c16898c2189c579c5f278a33319c
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-20 22:25:26 +0530

    Add files via upload

---
+++ funp5.c
@@ -0,0 +1,22 @@
+//write a program to accept a string  and print whether it is a palindrome or not
+
+
+
+#include<stdio.h>
+#include<string.c>
+int main()
+{
+
+	char name[10]="mayuri" ;
+	char name1[10]="miyu";
+if (strncmp(s1, s2, 8) ==0)
+{
+	printf("\n  string1 or string2 is equal");
+}
+else
+{
+	printf("\n string1 or string2 are different");
+}
+
+}
+
