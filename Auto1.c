069d41c63cd0dc290261741ba468b2f5ab25880f
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-22 15:04:36 +0530

    Add files via upload

---
+++ Auto1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ void displanum();
+ int main()
+ {
+ 	 auto int num1=20;
+ 	 int num2;
+ 	 printf(" num1=%d\n",num1);
+ 	 printf(" num2=%d\n",num2);
+ }
+ 
+ void displaynum()
+{
+//printf("\n num1 = %d",num1); //error since num1 and num2 is declared auto 
+//	printf("\n num2 = %d",num2);	
+}
+
