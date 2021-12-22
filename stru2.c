4bd8c6a61a8f21044f835724c365bb3361e55364
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-22 10:43:49 +0530

    Add files via upload

--- stru2.c
+++ stru2.c
@@ -10,17 +10,14 @@ struct book
  
   int main()
   {
-  	 struct book book1,book2;
-  	 book1.bookid=123;
-  	 strcpy(book1.tittle,"java");
-  	 book1.price=123.22f;
+    printf(" enter bookid");
+	scanf("%d",book1.bookid);
+	printf(" enter tittle");
+	scanf("%c",book1.tittle);
+	printf(" enter price");
+	scanf("%f",book1.price);
+	printf("\n -------------book details are -----------"); 	 
   	  printf(" \n bookid %d",book1.bookid);
   	  printf(" \n tittle %s",book1.tittle);
   	  printf("  \n price %f",book1.price);
-   book2.bookid=124;
-   strcpy(book2.tittle," html");
-   book2.price=124.22f;
-   printf("  \n bookid %d",book2.bookid);
-   printf("  \n tittle %s",book2.tittle);
-   printf(" \n price %f",book2.price);
 }
