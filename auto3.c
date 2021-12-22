1bdc3633097cb5d05321bae069b54e5643d01d3a
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-22 15:05:21 +0530

    Add files via upload

---
+++ auto3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void displaynum();
+int main()
+{
+	register int num1=20;
+	int num2;
+	printf(" \n num1=%d",num1);
+	printf("\n num2=%d",num2);
+	displaynum();
+	
+	
+	}
+	 void displaynum()
+	 {
+	 //	printf(" \n num1=%d",num1);
+	 //	printf("\n num2=%d",num2);
+	 }
+	 
