9dc6d4959078b067800bf242d7dc77d8b27dfe05
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-23 22:38:07 +0530

    Add files via upload

---
+++ enum7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+      
+    int main()
+	{  
+      enum fruits{mango = 1, strawberry=0, apple=0};  
+        printf("The value of mango is %d", mango);  
+        printf("\nThe value of apple is %d", apple);  
+      return 0;  
+    }  
+
