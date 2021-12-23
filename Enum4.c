81f372882987b0faa2c0d6313a8f1373208f842b
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-23 21:44:14 +0530

    Add files via upload

---
+++ Enum4.c
@@ -0,0 +1,11 @@
+
+  #include<stdio.h>
+    enum accounttype{ svaing=1,current,loan};
+    int main()
+    {
+    	 enum accounttype act;
+    	 act=loan;
+    	 printf( " enter  account type %d",act);
+    	 
+	}
+	
