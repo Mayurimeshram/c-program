2071c8203a6b0f9216c4274ea90c308a3c901f59
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-24 12:42:56 +0530

    Add files via upload

---
+++ Ass1.c
@@ -0,0 +1,15 @@
+// to calculate area and circumference
+ #include<stdio.h>
+ int main()
+ {
+ 	float a,c,r;
+ printf(" enter  radius");
+
+ r=4.2;
+ printf(" enter area");
+
+ a=3.14*r*r;
+ printf(" enter circumference");
+ c=2*3.14*r;
+ getch();
+}
