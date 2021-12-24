a2f4466ba8bc8c90949cb3eb5332844054be9106
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-24 12:59:25 +0530

    Add files via upload

---
+++ Array7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char name[5][10];
+int i;
+for(i=0;i<5;i++){
+	printf("enter your name ");
+scanf("%s",&name[i]);
+}
+
+//display the 5 names
+for(i=0;i<5;i++){
+printf("\nname is %s",name[i]);
+}
+}
+
+
